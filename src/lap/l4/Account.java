201f6ab52ca38f42061b80d71c64ea2d85ab559f
sidatube <70898455+sidatube@users.noreply.github.com>
2021-02-26 10:34:16 +0700

    ez

---
+++ src/lap/l4/Account.java
@@ -0,0 +1,65 @@
+package lap.l4;
+
+import java.util.Scanner;
+
+public class Account {
+    int id;
+    String name;
+    double balance;
+
+    public Account() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean credit(int a){
+        if(a>0){
+            setBalance(getBalance()+a);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean debit(int a){
+        if(a<=getBalance()){
+            setBalance(getBalance()-a);
+            return true;
+        }
+        return false;
+
+    }
+    public boolean tranFer(Account B){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Nhap so tien can chuyen: ");
+        int z= sc.nextInt();
+        if (this.debit(z)){
+
+            B.setBalance(getBalance()+z);
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ src/lap/l4/Main.java
@@ -0,0 +1,41 @@
+package lap.l4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Account a = new Account();
+        a.setId(1);
+        a.setName("NTC");
+        a.setBalance(200000);
+        Account b = new Account();
+        b.setId(2);
+        b.setName("Simp");
+        b.setBalance(100000);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so tien can nap:");
+        if(a.credit(sc.nextInt())){
+            System.out.println("Nap thanh cong");
+            System.out.println("So du hien co: "+a.getBalance());
+        }else {
+            System.out.println("Nap khong thanh cong");
+        }
+
+        System.out.println("Nhap so tien can tra:");
+        if (a.debit(sc.nextInt())){
+            System.out.println("Thanh toan thanh cong");
+            System.out.println("So du con lai: "+a.getBalance());
+        }else {
+            System.out.println("Thanh toan khong thanh cong");
+        }
+
+        if(a.tranFer(b)){
+            System.out.println("Chuyen khoan thanh cong");
+            System.out.println("So du con lai: "+a.getBalance());
+        }else
+            System.out.println("Chuyen khoan khong thanh cong");
+
+    }
+}
